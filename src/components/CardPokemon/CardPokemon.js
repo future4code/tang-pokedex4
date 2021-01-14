db843fe6a3f6144df53556d68bfd0e63155ee179
Bruna Pacheco <brunappacheco@hotmail.com>
2021-01-13 22:44:22 -0400

    layout pagina 1

--- src/components/CardPokemon/CardPokemon.js
+++ src/components/CardPokemon/CardPokemon.js
@@ -1,11 +1,26 @@
 import React from "react";
-
+import { MainCard, DivCard, DivImg, ButtonCard  } from "./styled";
+import imagemPikachu from "../img/pikachu.png";
+import imagemPokezinho from "../img/pokezinho.png";
 
 const Header = () => {
   return (
-    <div>
-        Cards
-    </div>
+    <MainCard>
+    <DivCard>
+      <DivImg>
+          <img src={imagemPikachu} alt="Pikachu" width="100px" height="150px" />
+      </DivImg>
+        <ButtonCard>Adicionar ao Pokedex </ButtonCard>
+        <ButtonCard>Ver detalhes</ButtonCard>
+    </DivCard>
+    <DivCard>
+      <DivImg>
+          <img src={imagemPokezinho} alt="Pokemon" width="100px" height="150px" />
+      </DivImg>
+        <ButtonCard>Adicionar ao Pokedex </ButtonCard>
+        <ButtonCard>Ver detalhes</ButtonCard>
+    </DivCard>
+    </MainCard>
   );
 };
 
---
+++ src/components/CardPokemon/styled.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const MainCard = styled.main`
+width: 100vw;
+height: 100vh;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-around;
+background-color: #626262;
+padding-top: 15px;
+    `
+export const DivCard = styled.div`
+background-color: rgb(241, 241, 241);
+display: grid;
+align-items: center;
+height: 45vh;
+width: 250px;
+`
+export const DivImg = styled.div`
+height: 150px;
+padding: 20px;
+padding-left: 70px;
+`
+export const ButtonCard = styled.button`
+border-top: 1px solid #000000;
+padding: 5px;
+height: 40px;
+width: 75%;
+margin-left: 30px; 
+background-color: #e64110;
+border-radius: 10px;
+color: whitesmoke;
+border-color: #e64110;
+`
+
+
